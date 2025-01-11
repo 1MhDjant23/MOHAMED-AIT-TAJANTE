a8283b3e8694abf6a289b6eb93ef53330f520b44
1MhDjant23 <mhdjant607@gmail.com>
2025-01-11 18:36:12 +0100

    mait-taj

---
+++ mait.js
@@ -0,0 +1,15 @@
+
+  (function(){
+     emailjs.init("ZpLa3gYmNska02rpx"); // Replace with your EmailJS user ID
+  })();
+
+  document.getElementById("contact-form").addEventListener("submit", function(event){
+      event.preventDefault();
+      
+      emailjs.sendForm("service_g0ez80i", "template_6au3c5i", this)
+        .then(function(response) {
+           alert("Message Sent Successfully!");
+        }, function(error) {
+           alert("Failed to send message. Please try again.");
+        });
+  });
